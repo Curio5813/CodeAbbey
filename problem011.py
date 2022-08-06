7412fcb669e36e958b5fa9bb1cffdefeffe59952
mfg0914 <marcosglasner@gmail.com>
2022-08-06 14:43:48 +0100

    Refactoring code of the problem 11, taken it from programing website CodeAbbey.

--- problem011.py
+++ problem011.py
@@ -1,29 +1,51 @@
 from csv import reader
 
 
-def sumOfDigits():
+def stringsToInteger():
     """
-    This function return the sum of digits given in a list
-    with three values with the first multipled by second and
-    the result of mutiplicantion is add to third number into list.
+    This function take the datas from a .csv in string file
+    to convert to integer.
     :return:
     """
     arq = open("problem011.csv")
     l1 = reader(arq, delimiter=" ")
     l1 = list(l1)
-    l2, l3, soma, somas, l4, l5 = [], [], 0, [], [], [],
-    for i in range(0, len(l1)):           # This loop take the values in a list of strings
-        for k in range(0, len(l1[i])):    # and convert it to integer.
+    l2, l3 = [], []
+    for i in range(0, len(l1)):
+        for k in range(0, len(l1[i])):
             l1[i][k] = int(l1[i][k])
             l2.append(l1[i][k])
         l3.append(l2)
         l2 = []
-    for i in range(0, len(l3)):           # This loop make the operations wih the terms and
-        for k in range(0, len(l3[i])):    # put this in a lits.
+    return l3
+
+
+def operationsIfTheData(l3):
+    """
+    This function has as return the operations with the three number given
+    by the "l3" list, wich the first is multipled by second and the result
+    of mutiplicantion is add to third number into list.
+    :param l3:
+    :return:
+    """
+    somas, soma = [], 0
+    for i in range(0, len(l3)):
+        for k in range(0, len(l3[i])):
             mult = l3[i][0] * l3[i][1]
             soma = mult + l3[i][2]
         somas.append(soma)
-    for i in range(0, len(somas)):        # This loop sum all digit that used to make a number
+    return somas
+
+
+def sumOfDigits(somas):
+    """
+    This function return the sum of the digits that is used by
+    the integer numbers given by the list "somas".
+    :param somas:
+    :return:
+    """
+    l4, l5 = [], []
+    for i in range(0, len(somas)):
         while somas[i] > 0:
             c = somas[i] % 10
             l4.append(c)
@@ -33,4 +55,4 @@ def sumOfDigits():
     return print(*l5)
 
 
-sumOfDigits()
+sumOfDigits(operationsIfTheData(stringsToInteger()))
