2656094716b54c757b7e3f480ad3d7d7bf805e58
Curio5813 <marcosglasner@gmail.com>
2022-12-17 19:06:09 +0000

    Refatorando o arquivo problem019.py

--- problem019.py
+++ problem019.py
@@ -1,9 +1,9 @@
 from csv import reader
 
 
-def justBrackets():
+def expressionsWithBrackets():
     """
-    This function open a .csv file and return a list of strings of brackets.
+    This function open a .csv file and return a list of strings with brackets.
     :return:
     """
     arq = open("problem019.csv")
@@ -13,6 +13,7 @@ def justBrackets():
     for i in range(0, len(l1)):
         for k in range(0, len(l1[i])):
             l2.append(l1[i][k])
+    print(l2)
     return l2
 
 
@@ -26,15 +27,24 @@ def matchingBrackets(l2):
     l3 = []
     for k in range(0, len(l2)):
         expressao = l2[k]
-        for i in range(len(expressao)):
-            ch = expressao[i]
+        for i in expressao:
+            ch = i
             if ch == '{' or ch == '[' or ch == '(' or ch == '<':
+                # Stacking the data
                 l3.append(ch)
             elif ch == '}' or ch == ']' or ch == ')' or ch == '>':
                 if len(l3) != 0:
-                    chx = str(l3.pop())
+                    # Unstack the data
+                    chx = str(l3.pop(-1))
                     if (ch == '}' and chx != '{') or (ch == ']' and chx != '[') or (ch == ')' and chx != '(') or \
                             (ch == '>' and chx != '<'):
+                        # To force the error
+                        l3.append(chx)
+                        break
+                elif len(l3) == 0:
+                    if ch == '}' or ch == ']' or ')' or ch == '>':
+                        # To force the error
+                        l3.append(ch)
                         break
         if len(l3) != 0:
             print(0, end=" ")
@@ -43,4 +53,4 @@ def matchingBrackets(l2):
         l3 = []
 
 
-matchingBrackets(justBrackets())
+matchingBrackets(expressionsWithBrackets())
