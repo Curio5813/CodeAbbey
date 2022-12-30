adc6949d7d3ecea93febb81ca2885f46d89c5244
Curio5813 <marcosglasner@gmail.com>
2022-12-30 01:09:35 +0000

    Add codeabbey.gif file and updating problem297.py

--- problem297.py
+++ problem297.py
@@ -7,17 +7,17 @@ def collatz_conjecture():
     This function treat about de Collatz Conjecture.
     :return:
     """
-    a, b, cont, lc = 35603679, 47792812, 0, []
+    a, b, cont1, l1 = 1492, 2018, 0, []
     for num in range(a, b + 1):
         while num != 1:
             if num % 2 == 0:
                 num /= 2
             else:
                 num = 3 * num + 1
-            cont += 1
-        lc.append(cont)
-        cont = 0
-    return print(max(lc))
+            cont1 += 1
+        l1.append(cont1)
+        cont1 = 0
+    return print(max(l1))
 
 
 collatz_conjecture()
