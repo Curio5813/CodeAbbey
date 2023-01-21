3dbe8379e9e74c1c3d335cc679923f882a1d9df3
Marcos Glasner <marcosglasner@gamil.com>
2023-01-21 13:08:08 +0000

    Changes in the directory .idea with installations of extensions for .csv files.

--- problem036.py
+++ problem036.py
@@ -29,6 +29,8 @@ def codeGuesser(l3):
     :return:
     """
     idx0_0, idx1_0, idx2_0 = [], [], []
+    idx0_1, idx1_1, idx2_1 = [], [], []
+    idx0_2, idx1_2, idx2_2 = [], [], []
     for i in range(0, len(l3)):
         for k in range(0, len(l3[i])):
             if l3[i][-1] == 0:
@@ -36,10 +38,26 @@ def codeGuesser(l3):
                 idx1_0.append(l3[i][1])
                 idx2_0.append(l3[i][2])
                 break
+            elif l3[i][-1] == 1:
+                idx0_1.append(l3[i][0])
+                idx1_1.append(l3[i][1])
+                idx2_1.append(l3[i][2])
+                break
+            elif l3[i][-1] == 2:
+                idx0_2.append(l3[i][0])
+                idx1_2.append(l3[i][1])
+                idx2_2.append(l3[i][2])
+                break
 
     print(idx0_0)
     print(idx1_0)
     print(idx2_0)
+    print(idx0_1)
+    print(idx1_1)
+    print(idx2_1)
+    print(idx0_2)
+    print(idx1_2)
+    print(idx2_2)
 
 
 codeGuesser(stringToInteger())
