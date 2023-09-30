d8526a9765abd172c72073285e3ecced1f6db7da
Curio5813 <marcosglasner@gmail.com>
2023-09-30 13:53:09 +0100

    Updating program.

--- problem081.py
+++ problem081.py
@@ -3,20 +3,27 @@ from csv import reader
 
 def bit_count():
     """
-    This function take an integer number and convert it to a binary number.
-    Then print the numbers of ones in each binary numbers converted. To
-    negative integer use the complement of 2 when convert it to binary numbers.
-    P.S.: The number of bit is equal to the number of "1" in that
-    represent the integer number in binary system.
+    This function open a .csv file with integer number and convert it all
+    to a binary number. Then print the numbers of 1 in each binary numbers
+    converted. To negative integer use the complement of 2 to convert binary
+    numbers with signal in a negative binary numbers.
+    P.S.: The number of bit is equal to the number of "1" and that represent
+    the integer number in binary numeric system.
     :return:
     """
+    # This snippet of the code open the .csv files with the datas
+    # of the program and put into a list.
     arq = open("problem081.csv")
     l1 = reader(arq, delimiter=" ")
     l1 = list(*l1)
+    # This snippet of the code convert the integer numbers in binary
+    # and put it into a list.
     l2, positivo, negativo, invertido, bit, bits, qt_bits, n1, n2 = [], [], [], [], "", [], [], 0, 0
     for i in range(0, len(l1)):
         l1[i] = int(l1[i])
         l2.append(bin(l1[i]))
+    # This snippet of the code divide positive binary's number from negative
+    # into a two different lists.
     for i in range(0, len(l2)):
         if l2[i][0] == "0":
             l2[i] = l2[i][2::]
@@ -26,6 +33,9 @@ def bit_count():
             negativo.append(l2[i])
             l2[i] = l2[i][::-1]
             invertido.append(l2[i])
+    # This snippet of the code use the complement of 2 to
+    # to convert binary numbers with signal in negative in
+    # negative binary numbers.
     for i in range(0, len(negativo)):
         for k in range(0, len(negativo[i])):
             while invertido[i][k] != "1":
@@ -47,6 +57,9 @@ def bit_count():
             bits.append(bit)
             bit = ""
             break
+    # This snippet of the code count how many 1 have in each
+    # binary numbers and put in a list that in the end of the
+    # program will print the quantitive of 1 for each binary.
     for i in range(0, len(l1)):
         if l1[i] >= 0:
             a = positivo[n1].count("1")
